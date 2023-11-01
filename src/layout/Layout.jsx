51f5dc6e7c1b70552b8ab4c89e7467c430ccdf4a
adilrion <adilmahmoudrion@gmail.com>
2023-11-02 03:17:40 +0600

    navbar initialize

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,14 +1,17 @@
 /* eslint-disable react/prop-types */
 
 import Footer from "../components/footer/footer"
-import TopNavbar from "../components/navigation/TopNavbar"
+import { TopNavbar } from "../components/navigation/TopNavbar"
 
 const Layout = ({ children }) => {
     return (
 
         <>
             <TopNavbar />
+            <div className="px-4 py-2 lg:px-8 lg:py-4 max-w-screen-2xl mx-auto">
             {children}
+
+            </div>
             <Footer />
         </>
     )
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,25 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/home/home";
+import { Gallery } from "../pages/gallery/Gallery";
+import { NotFoundPage } from "../pages/Error/NotFoundPage/NotFoundPage";
 
 const Router = createBrowserRouter([
     {
         path: "/",
         element: <Home/>,
-    }
+    },
+    {
+        path: "/home",
+element: <Home />,
+    },
+    {
+        path: "/gallery",
+        element: <Gallery />,
+    },
+    {
+        path: "*",
+        element: <NotFoundPage />,
+    },  
 ]);
 
 export default Router;

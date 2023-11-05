e256aa0b9c6edeb1beaa97823c247266aac9690c
adilrion <adilmahmoudrion@gmail.com>
2023-11-06 01:37:27 +0600

    hover effect and responsive design added

--- src/pages/gallery/Gindex.jsx
+++ src/pages/gallery/Gindex.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { Button, Spinner } from "@material-tailwind/react";
 import { firebaseStorage } from "../../firebase/firebaseStorage";
 import { useGallery } from '../../contexts/galleryContext/GalleryContext';
+import './GStyle.css'
 
 const GIndex = () => {
   const [selectedFile, setSelectedFile] = useState(null);
@@ -15,12 +16,23 @@ const GIndex = () => {
     setSelectedFile(event.target.files[0]);
   };
 
-  const handleToggleImageSelection = (imageName) => {
-    setSelectedImages((prevSelectedImages) => ({
-      ...prevSelectedImages,
-      [imageName]: !prevSelectedImages[imageName],
-    }));
+  const handleToggleImageSelection = (e, imageName) => {
+    setSelectedImages((prevSelectedImages) => {
+     
+      const newSelectedImages = { ...prevSelectedImages };
+
+    
+      if (e.target.checked) {
+        newSelectedImages[imageName] = true;
+      } else {
+       
+        delete newSelectedImages[imageName];
+      }
+
+      return newSelectedImages;
+    });
   };
+
 
   const handleUpload = async () => {
     if (selectedFile) {
@@ -43,7 +55,7 @@ const GIndex = () => {
           firebaseStorage.deleteImage(imageName)
         );
         await Promise.all(deletePromises);
-        console.log('Files deleted.');
+        setSelectedImages({})
         const updatedData = await firebaseStorage.fetchImages();
         setData(updatedData);
       } catch (error) {
@@ -62,6 +74,7 @@ const GIndex = () => {
 
   return (
     <section className="bg-white rounded-xl">
+    
       <div className="w-full flex justify-between items-center px-6 py-4 border-b-2">
         {
           numberOfSelectedImages <= 0 ? (<p className="text-[22px] font-bold">Gallery</p>) : (<div className="flex gap-3 text-[22px] font-bold">
@@ -69,6 +82,7 @@ const GIndex = () => {
               type="checkbox"
               checked={true}
               className="w-[22px]"
+              readOnly
             />
             <p>{numberOfSelectedImages} File Selected</p>
           </div>)
@@ -86,20 +100,25 @@ const GIndex = () => {
        
       </div>
 
-      <div className="grid grid-cols-5 gap-[25px] p-6">
+      <div className="grid grid-cols-2 lg:grid-cols-5 gap-[25px] p-6">
         {data.map((image, index) => (
-          <div key={index} className="col-span-1 border border-gray-500 rounded-xl overflow-hidden  relative first:col-span-2 first:row-span-2">
-            <img className=" w-full h-full aspect-square" src={image.url} alt={image.name} />
+          <div key={index} className="col-span-1 border border-gray-500 rounded-xl overflow-hidden  relative first:col-span-2 first:row-span-2 bg-effect">
+            <div className="absolute top-0 left-0 w-full h-full 
+            ">
+
+            </div>
+            <img className="w-full h-full aspect-square" src={image.url} alt={image.name} />
             <input
               type="checkbox"
               checked={selectedImages[image.name] || false}
-              onChange={() => handleToggleImageSelection(image.name)}
+              onChange={(e) => handleToggleImageSelection(e, image.name)}
               className="absolute top-2 left-2 z-50 w-[22px] h-[22px]"
             />
+          
           </div>
         ))}
-        <div className="flex items-center justify-center w-full">
-          <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center border-2 border-gray-300 border-dashed rounded-xl cursor-pointer bg-gray-50 dark:hover:bg-bray-800  hover:bg-gray-100 w-[213px] h-[213px]">
+        <div className="flex items-center justify-center w-full ">
+          <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center border-2 border-gray-300 border-dashed rounded-xl cursor-pointer  hover:bg-[#2f2f2f3f]  w-full h-full ">
             <div className="flex flex-col items-center justify-center ">
               <svg className="w-8 h-8 mb-4 text-gray-500 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
